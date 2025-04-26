4182836996c9228267c47d3097cf1da271aeabf5
CacTt4ck <lachainedelightning13090@gmail.com>
2025-04-26 21:45:18 +0200

    Initial commit

---
+++ src/main/java/com/tcaputi/back/stockncook/StockNCookApplication.java
@@ -0,0 +1,13 @@
+package com.tcaputi.back.stockncook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockNCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockNCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcaputi/back/stockncook/StockNCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcaputi.back.stockncook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockNCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
