87378d83ff0222e51f0e4f31707b70d55a139e24
Thomas C <lachainedelightning13090@gmail.com>
2025-05-18 20:44:55 +0200

    Graceful shutdown 30 min

--- src/main/java/com/tcaputi/back/stockncook/StockNCookApplication.java
+++ src/main/java/com/tcaputi/back/stockncook/StockNCookApplication.java
@@ -2,12 +2,23 @@ package com.tcaputi.back.stockncook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 public class StockNCookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockNCookApplication.class, args);
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setAwaitTerminationSeconds(1800);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
     }
 
 }
